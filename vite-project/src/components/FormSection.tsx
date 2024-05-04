0fa5a996f560cde97d5a4172f097ce789164c952
MeteBely <metehanmuradoglu17@gmail.com>
2024-05-05 01:03:36 +0300

    stil düzenlemeleri
    Ön yüz çalışması tamamlandı.

--- vite-project/src/components/FormSection.tsx
+++ vite-project/src/components/FormSection.tsx
@@ -12,29 +12,42 @@ const FormSection: React.FC<IFormProps> = ({ todos, setTodos }) => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setTitle("");
+    setContent("");
     if (todos !== null) {
-      setTodos((prev) => [...prev, { id: todos.length, title, content }]);
+      setTodos((prev) => [...prev, { id: todos.length + 1, title, content }]);
     }
   };
 
   return (
-    <section>
-      <form onSubmit={(e) => handleSubmit(e)}>
+    <section className="mb-8 font-serif mt-4">
+      <form
+        className="w-[800px] m-auto flex flex-col gap-2 items-center justify-center"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <label className="text-[18px]">Title</label>
         <input
+          className="outline-none border border-gray-400 rounded-sm w-[240px] px-2 mb-8"
           type="text"
           value={title}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setTitle(e.target.value)
           }
         />
-        <input
-          type="text"
+        <label className="text-[18px]">Content</label>
+        <textarea
+          className="outline-none border border-gray-400 rounded-sm resize-none w-full px-2 h-20 mb-4"
           value={content}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
             setContent(e.target.value)
           }
         />
-        <button type="submit">Submit</button>
+        <button
+          className="bg-red-400 px-12 py-2 rounded-md text-lg hover:bg-red-500"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </section>
   );
--- vite-project/src/components/TodoList.tsx
+++ vite-project/src/components/TodoList.tsx
@@ -7,15 +7,29 @@ interface ITodosProps {
 const TodoList: React.FC<ITodosProps> = ({ todos }) => {
   return (
     <section>
-      {todos &&
-        todos.length > 0 &&
-        todos.map((todo) => (
-          <div className="shadow-lg" key={todo.id}>
-            <h3>{todo.title}</h3>
-            <p>{todo.content}</p>
-            <span>{todo.id}</span>
-          </div>
-        ))}
+      <div className="flex flex-row gap-8 justify-start items-center w-[98%] m-auto flex-wrap">
+        {todos &&
+          todos.length > 0 &&
+          todos.map((todo) => (
+            <div className="shadow-xl w-[600px] px-2 h-[200px]" key={todo.id}>
+              <span className="text-lg font-semibold bg-red-400 rounded-full px-2 inline-block mb-2">
+                {todo.id}
+              </span>
+              <h3>
+                <span className="text-lg font-semibold text-red-600 mr-1">
+                  Title:
+                </span>
+                {todo.title}
+              </h3>
+              <p>
+                <span className="text-lg font-semibold text-red-600 mr-1">
+                  Content:
+                </span>
+                {todo.content}
+              </p>
+            </div>
+          ))}
+      </div>
     </section>
   );
 };
